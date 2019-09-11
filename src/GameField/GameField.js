8f253103c6551ab72120e6b973706473c67fb93c
tarasprocishin <taras1procishin@gmail.com>
2019-09-11 23:20:15 +0300

    Implement basick structure

---
+++ src/GameField/GameField.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const URL = 'http://starnavi-frontend-test-task.herokuapp.com';
+
+class GameField extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            data: {}
+        }
+        
+    }
+
+async componentDidMount() {
+        let date = await fetch(URL + '/game-settings');
+        let json = await date.json();
+
+        this.setState({ date: json})
+    }
+
+
+  
+
+    render() {
+
+        console.log(this.state.date)
+        return(
+            <h1>GameField</h1>
+        )
+    }
+ 
+}
+
+
+export default GameField;
---
+++ src/LeaderBoard/LeaderBoard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function LeaderBoard() {
+    return(
+        <h1>LeaderBoard</h1>
+    )
+
+}
+
+export default LeaderBoard;

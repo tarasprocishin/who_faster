f497fc7c397e7c9ab5574c92fafea92967250cf4
tarasprocishin <taras1procishin@gmail.com>
2019-09-18 15:00:34 +0300

    Updates

---
+++ precache-manifest.325068974fdf157c7aa50986452499b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f356c8c4427b936b8275d3b2bd0af622",
+    "url": "/who_faster/index.html"
+  },
+  {
+    "revision": "b79585ea6512eb3ae44e",
+    "url": "/who_faster/static/css/main.3f3eb5e4.chunk.css"
+  },
+  {
+    "revision": "3df81ca6b5df8235df50",
+    "url": "/who_faster/static/js/2.5bb09bca.chunk.js"
+  },
+  {
+    "revision": "b79585ea6512eb3ae44e",
+    "url": "/who_faster/static/js/main.faa28a30.chunk.js"
+  },
+  {
+    "revision": "4a6de0a8b432877dc570",
+    "url": "/who_faster/static/js/runtime~main.61b8f760.js"
+  }
+]);
